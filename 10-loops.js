0af021742dc946b0371ace87ab301e55b614578f
Yared Asmelash <yaredasmelash00@gmail.com>
2024-07-22 23:49:28 +0300

    Loops

--- 10-loops.js
+++ 10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const value of array) {
-      newArray.push(appendString + value);
+    const ar = [];
+    for (const val of array) {
+      ar.push(appendString + val);
     }
   
-    return newArray;
+    return ar;
   }
