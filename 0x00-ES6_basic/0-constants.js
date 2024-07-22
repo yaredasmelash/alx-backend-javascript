4cfbffc059413488307c0f861766424510b86987
Yared Asmelash <yaredasmelash00@gmail.com>
2024-07-22 22:50:20 +0300

    constants

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,14 @@
-export function taskNext() {
-    const combination = 'But sometimes let';
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let';
     combination += getLast();
   
     return combination;
