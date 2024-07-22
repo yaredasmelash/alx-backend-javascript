1863f91b3ec3981002b3ce9be588d943ee00a487
Yared Asmelash <yaredasmelash00@gmail.com>
2024-07-22 23:38:20 +0300

    Delete 0x00-ES6_basic directory

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const ar = [];
-    for (const val of array) {
-      ar.push(appendString + val);
-    }
-  
-    return ar;
-  }
